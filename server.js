9993300fe65fef97b8499d34ed39654115feb375
Michael Loughman <mcloughman@gmail.com>
2023-01-04 12:13:51 -0800

    Removed commented code. Desperate

--- server.js
+++ server.js
@@ -18,8 +18,8 @@ app.use((req, res, next) => {
 
 // routes
 // Need to take out the below lines. They are causing app to fail in heroku. I will implement full routes individually
-app.use(/*'/api/posts', */postRoutes)
-app.use(/*'/api/user', */userRoutes)
+app.use(postRoutes)
+app.use(userRoutes)
 
 mongoose.set('strictQuery', false)
 
